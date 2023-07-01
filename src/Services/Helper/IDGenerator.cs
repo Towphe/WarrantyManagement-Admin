ca4d16ac1ae00443ccd10a6110ccfb5afee8195f
Tope <castillochristopherjames@gmail.com>
2023-07-01 09:49:12 +0800

    created helper methods

---
+++ src/Services/Helper/IDGenerator.cs
@@ -0,0 +1,11 @@
+namespace src.Services;
+public static class IDGenerator{
+  public static string GenerateID(string type){
+    Random rng = Random.Shared;
+    
+    for (int i=0; i<8; i++){
+      type += (char)(rng.Next('A', 'Z' + 1));
+    }
+    return type;
+  }
+}
---
+++ src/Services/Helper/VerificationCodeGenerator.cs
@@ -0,0 +1,11 @@
+namespace src.Services;
+public static class VerificationCodeGenerator{
+  public static string GenerateCode(){
+    string code = "";
+    Random rng = Random.Shared;
+    for (int i=0; i<6; i++){
+      code += (char)(rng.Next('A', 'Z' + 1));
+    }
+    return code;
+  }
+}
