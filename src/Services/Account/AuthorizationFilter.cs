ea6ff48ce4513cf2813c9862712fc5a1906e1d56
Tope <castillochristopherjames@gmail.com>
2023-07-04 13:53:11 +0800

    created authorization filter

---
+++ src/Services/Account/AuthorizationFilter.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using src.Model.Repo;
+using src.Services.Account;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace src.Services.Account;
+
+public class AuthorizationFilter : IAsyncActionFilter{
+  public AuthorizationFilter(WarrantyrepoContext dbCtx){
+    _dbContext = dbCtx;
+  }
+  private WarrantyrepoContext _dbContext;
+  public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next){
+    User? user = await _dbContext.Users.Where(u => u.UserToken == context.HttpContext.Session.GetString("admin")).FirstOrDefaultAsync();
+    if (user == null){
+      // redirect to login page with message `not yet logged in`
+      context.HttpContext.Response.Redirect("/signin");
+    }
+    await next();
+  }
+}
