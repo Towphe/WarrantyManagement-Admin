b76086b0cba1f5aca7094415983a011f541359b6
Tope <castillochristopherjames@gmail.com>
2023-07-05 15:55:30 +0800

    created signup dto

---
+++ src/Model/Data/Account/SignUpDto.cs
@@ -0,0 +1,8 @@
+namespace src.Model.Data.Account;
+
+public class SignUpDto{
+  public string? Username {get; set;}
+  public string? Password {get; set;}
+  public string? ConfirmPassword {get; set;}
+  public string? Email {get; set;}
+}
