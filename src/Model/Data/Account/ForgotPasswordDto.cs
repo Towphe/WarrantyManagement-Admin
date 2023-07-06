76e209f1ab27e3de7fb07dbbca025b8000166edd
Tope <castillochristopherjames@gmail.com>
2023-07-06 11:24:14 +0800

    altered forgot password dto

--- src/Model/Data/Account/ForgotPasswordDto.cs
+++ src/Model/Data/Account/ForgotPasswordDto.cs
@@ -1,7 +1,7 @@
 namespace src.Model.Data.Account;
 
 public class ForgotPasswordDto{
-  public string? Id {get; set;}
+  public string? UserToken {get; set;}
   public string? Password {get; set;}
   public string? ConfirmPassword {get; set;}
 }
