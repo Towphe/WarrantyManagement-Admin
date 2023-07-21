342b021c85b8a43334c73a3a2a9c8368acf717a8
Tope <castillochristopherjames@gmail.com>
2023-07-21 11:44:02 +0800

    created simple entry dashboard

--- src/Controllers/DashboardController.cs
+++ src/Controllers/DashboardController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using src.Model.Data;
 using src.Model.Repo;
 using src.Services.Account;
+using Microsoft.EntityFrameworkCore;
 
 namespace src.Controllers;
 
@@ -24,5 +25,34 @@ public class DashboardController : Controller{
     ViewData["Title"] = "Dashboard";
     return View();
   }
+  [Route("Entries")]  
+  public async Task<IActionResult> Entries([FromQuery] int count = 10, [FromQuery] int page = 1){
+    ViewData["Title"] = "Entries";
+    //IEnumerable<Entry> entries = _dbContext.Entries.Skip((page - 1) * count).Take(count).AsEnumerable();
+    IEnumerable<Entry> entries = _dbContext.Entries.Skip((page - 1) * count).Take(count).Include(e => e.Product).AsEnumerable();
+    return View(entries);
+  }
+  [Route("Entries/{id}")]
+  public async Task<IActionResult> Entry([FromRoute] string id){
+    Entry? entry = await _dbContext.Entries.Where(e => e.Id == id).Include(e => e.Product).FirstAsync();
+    ViewData["Title"] = $"{id}";
+    return View(entry);
+  }
   // api
+  [Route("Approve/{id}")]
+  public async Task<IActionResult> Approve([FromRoute] string id){
+    Entry? entry = await _dbContext.Entries.FindAsync(id);
+    entry.Status = "ACCEPTED";
+    // email/contact user (TO FOLLOW)
+    await _dbContext.SaveChangesAsync();
+    return RedirectToAction(nameof(Entries));
+  }
+  [Route("Reject/{id}")]
+  public async Task<IActionResult> Reject([FromRoute] string id){
+    Entry? entry = await _dbContext.Entries.FindAsync(id);
+    entry.Status = "REJECTED";
+    // email/contact user (TO FOLLOW)
+    await _dbContext.SaveChangesAsync();
+    return RedirectToAction(nameof(Entries));
+  }
 }
